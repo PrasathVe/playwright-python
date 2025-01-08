c8c460d4893fa717bba3dfd0c323ecd70554bf79
PrasathVe <vijayst08@gmail.com>
2025-01-08 13:06:55 +0530

    tuple sample added

---
+++ Playwright_Python/Python_Samples/practice.py
@@ -0,0 +1,4 @@
+fruit_tuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruit_tuple[2:5]) #2 Starting index , 5 Ending Index
+
+#Output : ('cherry', 'orange', 'kiwi') (2,3,4)
