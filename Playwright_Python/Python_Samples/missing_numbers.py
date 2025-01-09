11bdc1c90bc1d849f6d35a9cb04791a99e8a9bce
PrasathVe <vijayst08@gmail.com>
2025-01-09 10:53:04 +0530

    missing numbers added

---
+++ Playwright_Python/Python_Samples/missing_numbers.py
@@ -0,0 +1,13 @@
+def find_missing_num(nums):
+    n = len(nums)+1
+    expected_sum = n*(n+1)//2
+    print(expected_sum)
+    actual_sum = sum(nums)
+    print(actual_sum)
+    return expected_sum - actual_sum
+
+
+nums = [1,2,3,5,6]
+missing_nums = find_missing_num(nums)
+print(f"Missing Number is: {missing_nums}")
+
