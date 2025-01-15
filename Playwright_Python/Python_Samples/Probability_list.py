3a9536bc900077210677f4d7d15858eade30e7b4
PrasathVe <vijayst08@gmail.com>
2025-01-15 20:19:27 +0530

    probablity added

--- Playwright_Python/Python_Samples/Probability_list.py
+++ Playwright_Python/Python_Samples/Probability_list.py
@@ -1,7 +1,7 @@
-x = int(input())
-y = int(input())
-z = int(input())
-n = int(input())
+x = int(input("Enter 1st Number: "))
+y = int(input("Enter 2nd Number: "))
+z = int(input("Enter 3rd Number: "))
+n = int(input("Enter Number of Combinations: "))
 mylist = x,y,z
 for i in range(n):
     for j in range(n):
